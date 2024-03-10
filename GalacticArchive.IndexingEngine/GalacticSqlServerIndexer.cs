957842cdda19548b3ffa1447a47997966f6e2afb
ArsenPetrosian <114424846+ArsenPetrosian@users.noreply.github.com>
2024-03-10 13:21:14 +0000

    Added PSET2 solution

--- GalacticArchive.IndexingEngine/GalacticSqlServerIndexer.cs
+++ GalacticArchive.IndexingEngine/GalacticSqlServerIndexer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Channels;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -14,16 +15,100 @@ public class GalacticSqlServerIndexer
         _logger = logger;
         _options = options.Value;
     }
-        
+
     public async Task RunAsync(ChannelReader<FileSystemInfo> channelReader, CancellationToken cancellationToken)
     {
-        var connectionString = _options.ConnectionString;
-        
-        // Channels are an advanced topic, but if interested, you can learn about them here
-        // https://devblogs.microsoft.com/dotnet/an-introduction-to-system-threading-channels/
-        await foreach (FileSystemInfo item in channelReader.ReadAllAsync(cancellationToken))
+        string connectionString = _options.ConnectionString;
+
+        try
         {
-            // Good luck!
+            await foreach (FileSystemInfo item in channelReader.ReadAllAsync(cancellationToken))
+            {
+                await ProcessFileSystemInfoAsync(item, connectionString, cancellationToken);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while indexing file system information.");
+        }
+    }
+
+    private async Task ProcessFileSystemInfoAsync(FileSystemInfo info, string connectionString, CancellationToken cancellationToken)
+    {
+        if (info is DirectoryInfo directory)
+        {
+            await ProcessDirectoryAsync(directory, connectionString, cancellationToken);
+        }
+        else if (info is FileInfo file)
+        {
+            await ProcessFileAsync(file, connectionString, cancellationToken);
+        }
+    }
+
+    private async Task ProcessDirectoryAsync(DirectoryInfo directory, string connectionString, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Processing directory: {directory.Name}");
+
+        string path = directory.FullName;
+        string name = directory.Name;
+        string directoryPath = directory.FullName;
+        DateTime lastModified = DateTime.UtcNow;
+
+        string query = "IF NOT EXISTS (SELECT TOP 1 FROM Directories WHERE [Path] = @Path) " +
+                        "BEGIN " +
+                        "INSERT INTO Directories ([Path], [Name], [DirectoryPath], [LastModified]) " +
+                        "VALUES (@Path, @Name, @DirectoryPath, @LastModified) " +
+                        "END " +
+                        "ELSE " +
+                        "BEGIN " +
+                        "UPDATE Directories SET [Name] = @Name, [DirectoryPath] = @DirectoryPath, [LastModified] = @LastModified " +
+                        "WHERE [Path] = @Path " +
+                        "END ";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Path", path);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@DirectoryPath", directoryPath);
+                command.Parameters.AddWithValue("@LastModified", lastModified);
+
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+
+    private async Task ProcessFileAsync(FileInfo file, string connectionString, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Processing file: {file.Name}");
+        string path = file.FullName;
+        string name = file.Name;
+        string directoryPath = file.FullName;
+        long size = file.Length;
+        DateTime lastModified = DateTime.UtcNow;
+
+        string query = "IF NOT EXISTS (SELECT TOP 1 FROM Files WHERE [Path] = @Path) " +
+                        "BEGIN " +
+                        "INSERT INTO Files ([Path], [Name], [DirectoryPath], [Size], [LastModified]) " +
+                        "VALUES (@Path, @Name, @DirectoryPath, @Size, @LastModified) " +
+                        "END " +
+                        "ELSE " +
+                        "BEGIN " +
+                        "UPDATE Files SET [Name] = @Name, [DirectoryPath] = @DirectoryPath, [Size] = @Size, [LastModified] = @LastModified " +
+                        "WHERE [Path] = @Path " +
+                        "END ";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Path", path);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@DirectoryPath", directoryPath);
+                command.Parameters.AddWithValue("@Size", size);
+                command.Parameters.AddWithValue("@LastModified", lastModified);
+
+                await command.ExecuteNonQueryAsync();
+            }
         }
     }
 }
