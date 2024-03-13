fee42c4b9dfcfb8693a5774a81a21043770fb2f0
DavitD1996 <davit.sargsyan@picsart.com>
2024-03-13 18:54:53 +0400

    Galaxy system implementation

--- GalacticArchive.IndexingEngine/initDbSchema.sql
+++ GalacticArchive.IndexingEngine/initDbSchema.sql
@@ -1,1 +1,22 @@
-﻿
+﻿Create Table Directories(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+[Path]VARCHAR(256) NOT NULL,
+[DirectoryPath] VARCHAR(256) NOT NULL,
+[Name] VARCHAR(256) NOT NULL,
+LastModified DATETIME NOT NULL
+)
+Create Table Files(
+Id int identity(1,1) Primary key,
+[Path] VARCHAR(256) NOT NULL,
+[DirectoryPath] VARCHAR(256) NOT NULL,
+[Name] VARCHAR(256) NOT NULL,
+LastModified DATETIME NOT NULL,
+Size BIGINT NOT NULL,
+DirectoryId INT FOREIGN KEY REFERENCES Directories(Id)
+)
+
+CREATE INDEX Directory_Path
+ON Directories([Path],[Name])
+
+CREATE INDEX File_Path
+ON Files([Path],[Name])
